02ee71c36d15326942c8640829d5ff573df63741
Aaron Walker <awwalker3@gmail.com>
2015-11-21 18:09:17 -0500

    add given files

--- src/BST.java
+++ src/BST.java
@@ -1,9 +1,20 @@
 /**
  * This class implements a binary search tree.
- * All of its methods are implemented recusively
+ * All of its methods are implemented recursively
  * @author aaronwalker
  *
  */
-public class BST<E> implements BSTInterface {
+public class BST<E extends Comparable <E> > implements BSTInterface<E> {
+	public void insert ( E item ){
+		
+	}
 	
+	public void remove ( E item ){
+		
+	}
+	
+	public boolean contains ( E item ){
+		boolean truth = true;
+		return truth;
+	}
 }
---
+++ src/DictionaryInterface.java
@@ -0,0 +1,26 @@
+/**
+ * This interface defines the methods needed
+ * to be implemented by the Dictionary class
+ * @author Joanna Klukowska
+ */
+
+public interface DictionaryInterface {
+	/**
+	 * This method determines if a given word 
+	 * is in this Dictionary.
+	 * @param word the word to be checked
+	 * @return true of the word is in this Dictionary,
+	 * false otherwise
+	 */
+	boolean findWord ( String word );
+	
+	/**
+	 * This method determines if a given
+	 * prefix is a prefix of a word that exists in
+	 * this Dictionary.
+	 * @param prefix the prefix to be checked  
+	 * @return true if the prefix is in this Dictionary,
+	 * false otherwise 
+	 */
+	boolean findPrefix ( String prefix ); 
+}
