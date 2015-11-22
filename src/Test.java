735ed81e1087750c52c4a9ef370c55ad4378287a
Aaron Walker <awwalker3@gmail.com>
2015-11-21 20:21:36 -0500

    test empty

--- src/Test.java
+++ src/Test.java
@@ -8,17 +8,7 @@
 public class Test extends BST<Integer> {
 	public static void main ( String[] args ){
 		BST<Integer> nums = new BST<Integer>();
-		nums.insert(25);
-		nums.insert(10);
-		nums.insert(29);
-		nums.insert(7);
-		nums.insert(15);
-		nums.insert(10);
-		nums.insert(17);
-		nums.insert(25);
-		nums.insert(35);
-		nums.insert(30);
-		nums.insert(40);
+		
 		System.out.println(nums.toString());
 	}
 }
