78cf1bbe9d4af44c9faeec1d1c1e798f4a5b3102
Aaron Walker <awwalker3@gmail.com>
2015-11-26 03:11:29 -0500

    change methods

--- src/BST.java
+++ src/BST.java
@@ -10,6 +10,47 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 	public BST(){
 		this.root = null;
 	}
+	public int getHeight ( BSTNode<E> node){
+		if ( node == null ){
+			return 0;
+		}
+		return node.height;
+	}
+	
+	public int getBalance ( BSTNode<E> node){
+		if ( node == null ){
+			return 0;
+		}
+		return getHeight( node.left ) - getHeight( node.right );
+	}
+	
+	private BSTNode<E> leftRotate( BSTNode<E> node){
+		//temporary for rotation
+		BSTNode<E> subLeft = node.left;
+		BSTNode<E> subLeftRight = subLeft.right;
+		//perform rotation
+		subLeft.right = node;
+		node.left = subLeftRight;
+		//update heights
+		node.height = Math.max( getHeight( node.left ), getHeight( node.right ) ) + 1;
+		subLeft.height = Math.max( getHeight( subLeft.left ), getHeight( subLeft.right ) ) + 1;
+		
+		return subLeft; //new root
+	}
+	
+	private BSTNode<E> rightRotate( BSTNode<E> node){
+		//temporary for rotation
+		BSTNode<E> subRight = node.right;
+		BSTNode<E> subRightLeft = subRight.left;
+		//perform rotation
+		subRight.left = node;
+		node.right = subRightLeft;
+		//update heights
+		node.height = Math.max( getHeight( node.left ), getHeight( node.right ) ) + 1;
+		subRight.height = Math.max( getHeight( subRight.left ), getHeight( subRight.right ) ) + 1;
+		
+		return subRight; //new root
+	}
 	
 	public void insert ( E item ){
 		root = recInsert ( root, item);
@@ -17,39 +58,84 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 	
 	private BSTNode<E> recInsert ( BSTNode<E> node, E newData ){
 		if ( node == null ){
-			BSTNode<E> newNode = new BSTNode<E>( newData );
-			return newNode;
+			node = new BSTNode<E>( newData );
 		}
-		if ( newData.compareTo(node.data) < 0 ){
+		else if ( newData.compareTo(node.data) < 0 ){
 			node.left = recInsert ( node.left, newData );
 		}
 		else{
 			node.right = recInsert ( node.right, newData );
 		}
-		node.height = node.max( node.getHeight( node.left ), node.getHeight( node.right ) ) + 1;
+		node.height = Math.max( getHeight( node.left ), getHeight( node.right ) ) + 1;
 		
-		int balance = node.getBalance( node );
+		int balance = getBalance( node );
 		
-		if( balance > 1 && newData.compareTo( node.left.data ) < 0 ){
-			return node.rightRotate( node );
+		//right tree is heavy
+		if( balance == 2 && newData.compareTo( node.left.data ) < 0 ){
+			return leftRotate( node );
 		}
-		if( balance < -1 && newData.compareTo( node.right.data ) > 0 ){
-			return node.leftRotate( node );
+		//left tree is heavy
+		if( balance == -2 && newData.compareTo( node.right.data ) > 0 ){
+			return rightRotate( node );
 		}
-		if( balance > 1 && newData.compareTo( node.left.data ) > 0 ){
-			node.left = node.leftRotate( node.left );
-			return node.rightRotate( node );
+		
+		if( balance == 2 && newData.compareTo( node.left.data ) > 0 ){
+			node.left = rightRotate( node.left );
+			return leftRotate( node );
 		}
-		if( balance < -1 && newData.compareTo( node.right.data ) < 0 ){
-			node.right = node.rightRotate( node.right );
-			return node.leftRotate( node );
+		if( balance == -2 && newData.compareTo( node.right.data ) < 0 ){
+			node.right = leftRotate( node.right );
+			return rightRotate( node );
 		}
 		
 		return node;
 		
-		
 	}
 	
+	/*
+	 * @param tree the root of the current subtree
+	 * @param level level (depth) of the current recursive call in the tree
+	 * to determine the indentation of each item 
+	 * @param output the string that accumulated the string representation
+	 * of this BST
+	 */
+	private void preOrderPrint(BSTNode<E> tree, int level, StringBuilder output)
+	{
+		if (tree != null) {
+			String spaces = "\n";
+			if (level > 0) {
+				for (int i = 0; i < level - 1; i++)
+					spaces += "   ";
+				spaces += "|--";
+			}
+			output.append(spaces);
+			output.append(tree.data);
+			preOrderPrint(tree.left, level + 1, output);
+			preOrderPrint(tree.right, level + 1, output);
+		}
+		//uncomment the part below to show "null children" in the output 
+//		else {
+//			String spaces = "\n";
+//			if (level > 0) {
+//				for (int i = 0; i < level - 1; i++)
+//					spaces += "   ";
+//				spaces += "|--";
+//			}
+//			output.append(spaces);
+//			output.append("null");
+//		}
+	}
+
+	/**
+	 * Produces tree like string representation of this BST.
+	 * @return string containing tree-like representation of this BST. 
+	 */
+	public String toStringTreeFormat() {
+		StringBuilder s = new StringBuilder();
+
+		preOrderPrint(root, 0, s);
+		return s.toString();
+	}
 
 	public void remove ( E item ){
 		
@@ -59,20 +145,6 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 		boolean truth = true;
 		return truth;
 	}
-	
-	private int recSize (BSTNode<E> root){
-		if(root == null){
-			return 0;
-		}
-		else{
-			return recSize(root.left) + recSize(root.right) + 1;
-		}
-	}
-	
-	public int size(){
-		return recSize(root);
-	}
-	
 	
 	@SuppressWarnings("hiding")
 	protected class BSTNode <E extends Comparable <E> > {
@@ -89,54 +161,11 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 			this.height = 1;
 		}
 		
-		public int max (int a, int b){
-			return ( a > b ) ? a : b; 
-		}
-		
 		public int compareTo ( BSTNode <E> other){
 			return this.data.compareTo(other.data);
 		}
 		
-		public int getHeight ( BSTNode<E> node){
-			if ( node == null ){
-				return 0;
-			}
-			return node.height;
-		}
 		
-		public int getBalance ( BSTNode<E> node){
-			if ( node == null ){
-				return 0;
-			}
-			return getHeight( node.left ) - getHeight( node.right );
-		}
-		
-		private BSTNode<E> rightRotate( BSTNode<E> node){
-			//temporary for rotation
-			BSTNode<E> subLeft = node.left;
-			BSTNode<E> subLeftRight = subLeft.right;
-			//perform rotation
-			subLeft.right = node;
-			node.left = subLeftRight;
-			//update heights
-			node.height = max( getHeight( node.left ), getHeight( node.right ) ) + 1;
-			subLeft.height = max( getHeight( subLeft.left ), getHeight( subLeft.right ) ) + 1;
-			
-			return subLeft; //new root
-		}
-		
-		private BSTNode<E> leftRotate( BSTNode<E> node){
-			BSTNode<E> subRight = node.right;
-			BSTNode<E> subRightLeft = subRight.left;
-			
-			subRight.left = node;
-			node.right = subRightLeft;
-			
-			node.height = max( getHeight( node.left ), getHeight( node.right ) ) + 1;
-			subRight.height = max( getHeight( subRight.left ), getHeight( subRight.right ) ) + 1;
-			
-			return subRight;
-		}
 	}
 	
 }
