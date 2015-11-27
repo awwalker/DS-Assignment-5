6fb8fd37c8114fc3205b7c7e7fba49da37696a5c
Aaron Walker <awwalker3@gmail.com>
2015-11-27 06:09:06 -0500

    yay

--- src/BST.java
+++ src/BST.java
@@ -18,80 +18,193 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 	}
 	
 	public int getBalance ( BSTNode<E> node){
-		if ( node == null ){
-			return 0;
+		if ( node.right == null ){
+			return -node.height;
 		}
-		return getHeight( node.left ) - getHeight( node.right );
+		if ( node.left == null ){
+			return node.height;
+		}
+		return node.right.height - node.left.height;
 	}
 	
-	private BSTNode<E> leftRotate( BSTNode<E> node){
+	private BSTNode<E> leftRotate( BSTNode<E> node){//deal with LL Imbalance
 		//temporary for rotation
 		BSTNode<E> subLeft = node.left;
-		BSTNode<E> subLeftRight = subLeft.right;
 		//perform rotation
+		
+		node.left = subLeft.right;
 		subLeft.right = node;
-		node.left = subLeftRight;
-		//update heights
-		node.height = Math.max( getHeight( node.left ), getHeight( node.right ) ) + 1;
-		subLeft.height = Math.max( getHeight( subLeft.left ), getHeight( subLeft.right ) ) + 1;
+		
+		updateHeight(node);
+		updateHeight(subLeft);
 		
 		return subLeft; //new root
 	}
 	
-	private BSTNode<E> rightRotate( BSTNode<E> node){
+	private BSTNode<E> rightRotate( BSTNode<E> node){//deal with RR Imbalance
 		//temporary for rotation
 		BSTNode<E> subRight = node.right;
-		BSTNode<E> subRightLeft = subRight.left;
+		
 		//perform rotation
+		node.right = subRight.left;
 		subRight.left = node;
-		node.right = subRightLeft;
+		
 		//update heights
-		node.height = Math.max( getHeight( node.left ), getHeight( node.right ) ) + 1;
-		subRight.height = Math.max( getHeight( subRight.left ), getHeight( subRight.right ) ) + 1;
+		updateHeight(node);
+		updateHeight(subRight);
 		
 		return subRight; //new root
 	}
 	
-	public void insert ( E item ){
-		root = recInsert ( root, item);
+	private BSTNode<E> leftRightRotate( BSTNode<E> node){
+		BSTNode<E> subLeft = node.left;
+		BSTNode<E> subLeftRight = subLeft.right;
+		
+		node.left = subLeftRight.right;
+		subLeft.right = subLeftRight.left;
+		subLeftRight.left = subLeft;
+		subLeftRight.right = node;
+		
+		updateHeight(node);
+		updateHeight(subLeft);
+		updateHeight(subLeftRight);
+		
+		return subLeftRight;
 	}
 	
-	private BSTNode<E> recInsert ( BSTNode<E> node, E newData ){
+	private BSTNode<E> rightLeftRotate( BSTNode<E> node){
+		BSTNode<E> subRight = node.right;
+		BSTNode<E> subRightLeft = subRight.left;
+		
+		node.right = subRightLeft.left;
+		subRight.left = subRightLeft.right;
+		subRightLeft.right = subRight;
+		subRightLeft.left = node;
+		
+		updateHeight(node);
+		updateHeight(subRight);
+		updateHeight(subRightLeft);
+		
+		return subRightLeft;
+	}
+	
+	public void insert ( E item ){
+		root = insert ( root, item);
+	}
+	
+	private BSTNode<E> insert ( BSTNode<E> node, E newData ){
 		if ( node == null ){
-			node = new BSTNode<E>( newData );
+			node = new BSTNode<E>( newData, null, null );
 		}
 		else if ( newData.compareTo(node.data) < 0 ){
-			node.left = recInsert ( node.left, newData );
+			node.left = insert ( node.left, newData );
 		}
 		else{
-			node.right = recInsert ( node.right, newData );
-		}
-		node.height = Math.max( getHeight( node.left ), getHeight( node.right ) ) + 1;
-		
-		int balance = getBalance( node );
-		
-		//right tree is heavy
-		if( balance == 2 && newData.compareTo( node.left.data ) < 0 ){
-			return leftRotate( node );
-		}
-		//left tree is heavy
-		if( balance == -2 && newData.compareTo( node.right.data ) > 0 ){
-			return rightRotate( node );
+			node.right = insert ( node.right, newData );
 		}
 		
-		if( balance == 2 && newData.compareTo( node.left.data ) > 0 ){
-			node.left = rightRotate( node.left );
-			return leftRotate( node );
-		}
-		if( balance == -2 && newData.compareTo( node.right.data ) < 0 ){
-			node.right = leftRotate( node.right );
-			return rightRotate( node );
-		}
-		
+		updateHeight(node);
+		node = balance(node);
 		return node;
 		
 	}
 	
+	public void remove ( E data ){
+		if( data != null ){
+			root = remove( data, root );
+		}
+	}
+	
+	private BSTNode<E> remove( E data, BSTNode<E> node ){
+		if( node == null ){
+			;
+		}
+		if( data.compareTo( node.data ) < 0 ){
+			node.left = remove( data, node.left );
+			updateHeight(node);
+			return balance(node);
+		}
+		else if ( data.compareTo( node.data ) > 0 ){
+			node.right = remove( data, node.right );
+			updateHeight(node);
+			return balance(node);
+		}
+		else{
+			if( node.left == null && node.right == null ){
+				return null;
+			}
+			
+			if( node.left == null ){
+				return node.right;
+			}
+			
+			if( node.right == null ){
+				return node.left;
+			}
+			
+			else{
+				E predValue = getPredecessor(node.left);
+				node.data = predValue;
+				node.left = remove( predValue, node.left );
+				updateHeight(node);
+				return balance(node);
+			}
+		}
+
+	}
+
+	private BSTNode<E> balance( BSTNode<E> node){
+		
+		if( getBalance(node) == -2){
+			BSTNode<E> t = tallerSub(node.left, node.right);
+			if(getBalance(t) == -1 || getBalance(t) == 0){
+				node = leftRotate(node);
+			}
+			else if (getBalance(t)== 1){
+				node = leftRightRotate(node);
+			}
+		}
+		if( getBalance( node ) == 2){
+			BSTNode<E> t = tallerSub(node.left, node.right);
+			if(getBalance(t) == 0 || getBalance(t) == 1){
+				node = rightRotate(node);
+			}
+			else if(getBalance(t)== -1 ){
+				node = rightLeftRotate(node);
+			}
+		}
+		return node;
+	}
+	
+	private BSTNode<E> tallerSub( BSTNode<E> left, BSTNode<E> right){
+		if( left == null ){
+			return right;
+		}
+		else if( right == null ){
+			return left;
+		}
+		if( left.height >= right.height ){
+			return left;
+		}
+		else{
+			return right;
+		}
+	}
+	
+	private void updateHeight(BSTNode<E> node){
+		if(node.left == null && node.right == null){
+			node.height = 0;
+		}
+		else if(node.left == null){
+			node.height = node.right.height + 1;
+		}
+		else if(node.right == null){
+			node.height = node.left.height + 1;
+		}
+		else{
+			node.height = Math.max(node.left.height, node.right.height) + 1;
+		}
+	}
 	/*
 	 * @param tree the root of the current subtree
 	 * @param level level (depth) of the current recursive call in the tree
@@ -99,7 +212,7 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 	 * @param output the string that accumulated the string representation
 	 * of this BST
 	 */
-	private void preOrderPrint(BSTNode<E> tree, int level, StringBuilder output)
+	private void postOrderPrint(BSTNode<E> tree, int level, StringBuilder output)
 	{
 		if (tree != null) {
 			String spaces = "\n";
@@ -110,8 +223,8 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 			}
 			output.append(spaces);
 			output.append(tree.data);
-			preOrderPrint(tree.left, level + 1, output);
-			preOrderPrint(tree.right, level + 1, output);
+			postOrderPrint(tree.left, level + 1, output);
+			postOrderPrint(tree.right, level + 1, output);
 		}
 		//uncomment the part below to show "null children" in the output 
 //		else {
@@ -130,41 +243,78 @@ public class BST<E extends Comparable <E> > implements BSTInterface<E>{
 	 * Produces tree like string representation of this BST.
 	 * @return string containing tree-like representation of this BST. 
 	 */
-	public String toStringTreeFormat() {
+	public String toString() {
 		StringBuilder s = new StringBuilder();
 
-		preOrderPrint(root, 0, s);
+		postOrderPrint(root, 0, s);
 		return s.toString();
 	}
-
-	public void remove ( E item ){
-		
+	
+	private E getPredecessor( BSTNode<E> node ){
+		while( node.right != null ){
+			node = node.right;
+		}
+		return node.data;
 	}
 	
-	public boolean contains ( E item ){
-		boolean truth = true;
-		return truth;
+	public boolean contains ( E data ){
+		if ( data != null ){
+			return contains( data, root);
+		}
+		return false;
+	}
+	
+	private boolean contains( E data, BSTNode<E> node){
+		//should just be like a binary search
+		//reached end of tree and never found it
+		if( node == null ){
+			return false;
+		}
+		//check out left sub tree
+		else if( data.compareTo( node.data ) < 0 ){
+			return contains( data, node.left );
+		}
+		//check out right subtree
+		else if( data.compareTo( node.data ) > 0 ){
+			return contains( data, node.right );
+		}
+		//found it
+		return true;
 	}
 	
 	@SuppressWarnings("hiding")
 	protected class BSTNode <E extends Comparable <E> > {
 		
-		private E data;
-		private BSTNode <E> left;
-		private BSTNode <E> right;
-		private int height;
+		protected E data;
+		protected BSTNode <E> left;
+		protected BSTNode <E> right;
+		protected int height;
 		
 		public BSTNode ( E data ){
+			this(data, null, null);
+		}
+		
+		public BSTNode ( E data, BSTNode<E> left, BSTNode<E> right){
 			this.data = data;
-			this.left = null;
-			this.right = null;
-			this.height = 1;
+			this.left = left;
+			this.right = right;
+			if( left == null && right == null ){
+				this.height = 0;
+			}
+			else if( left == null ){
+				this.height = 1 + right.height;
+			}
+			else if( right == null ){
+				this.height = 1 + left.height;
+			}
+			else{
+				this.height = 1 + Math.max(left.height, right.height);
+			}
 		}
 		
 		public int compareTo ( BSTNode <E> other){
 			return this.data.compareTo(other.data);
 		}
-		
 		
 	}
 	
--- src/Test.java
+++ src/Test.java
@@ -8,12 +8,17 @@
 public class Test extends BST<Integer> {
 	public static void main ( String[] args ){
 		BST<Integer> nums = new BST<Integer>();
-		Integer[] toAdd = {2,4,7};
+		
+		Integer[] toAdd = {1,2,3, 4, 5,6, 7,8};
 		for(int num : toAdd){
 			nums.insert(num);
 		}
 		
-		System.out.println(nums.toStringTreeFormat());
-	
+		System.out.println(nums.toString());
+		
+		System.out.println("\n\n");
+		
+		nums.remove(4);
+		System.out.println(nums.toString());
 	}
 }
